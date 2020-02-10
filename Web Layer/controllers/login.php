5a0e7a2b4c24a1eab7d639218c229b15e2e0b05e
Zdravko Hvarlingov <hvurlingov@uni-sofia.bg>
2020-02-11 00:28:53 +0200

    Login validation inside AuthorizationService

--- Web Layer/controllers/login.php
+++ Web Layer/controllers/login.php
@@ -5,22 +5,16 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: ' . 'Access-Control-Allow-Headers, Content-Type, ' . 'Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once('../dtos/LoginDto.php');
-include_once('../../Domain Layer/services/LoginValidationService.php');
-include_once('../../Domain Layer/services/LoginPerformerService.php');
 include_once('../../Domain Layer/services/AuthorizationService.php');
 
-$loginValidationService = new LoginValidationService();
-$loginPerformerService = new LoginPerformerService();
 $authorizationService = new AuthorizationService();
 
 $data = json_decode(file_get_contents('php://input'));
-
 $loginDto = new LoginDto($data->email, $data->password);
-try {
-    $loginValidationService->validateCredentials($loginDto);
-    $user = $loginPerformerService->getLoggedUser($loginDto);
 
-    $authorizationService->login($user);
+try {
+    $authorizationService->login($loginDto);
+
     header('Location: ' . '..\\..\\client\\views\\index.php');
     http_response_code(302);
 } catch (Exception $ex) {
