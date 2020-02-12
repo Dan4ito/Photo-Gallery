4f33c49f83e7047df33b6f51c80c3256857775fb
Dan4ito <yordan.petkov.1997@gmail.com>
2020-02-12 02:21:25 +0200

    Improving add images controller

--- Domain Layer/services/GalleryService.php
+++ Domain Layer/services/GalleryService.php
@@ -6,6 +6,8 @@ include_once('../../Data Layer/repositories/ImageGalleryRepository.php');
 include_once('../../Domain Layer/services/GalleryValidationService.php');
 include_once('../../Domain Layer/services/AuthorizationService.php');
 include_once('../../Domain Layer/services/GalleryUserValidatorService.php');
+include_once('../../Domain Layer/services/ImageValidationService.php');
+include_once('../../Domain Layer/services/ImageUploadService.php');
 
 class GalleryService
 {
@@ -16,6 +18,8 @@ class GalleryService
     private $galleryUserValidatorService;
     private $imageRepository;
     private $imageGalleryRepository;
+    private $imageValidationService;
+    private $imageUploadService;
 
     function __construct()
     {
@@ -25,12 +29,14 @@ class GalleryService
         $this->galleryUserValidatorService = new GalleryUserValidatorService();
         $this->imageRepository = new ImageRepository();
         $this->imageGalleryRepository = new ImageGalleryRepository();
+        $this->imageValidationService = new ImageValidationService();
+        $this->imageUploadService = new ImageUploadService();
     }
 
     public function MergeGalleries($galleryName, $galleryIds)
     {
         $this->galleryValidationService->validateGallery($galleryName);
-        if($this->galleryUserValidatorService->canUserViewGalleries($galleryIds)){
+        if ($this->galleryUserValidatorService->canUserViewGalleries($galleryIds)) {
             $createdGalleryId = $this->galleryRepository->Create($galleryName, $this->authorizationService->getLoggedInUser()->id);
             $imagesForAllGalleries = $this->imageRepository->GetImagesForGalleries($galleryIds);
             $imagesIds = array_map(function ($image) {
@@ -38,6 +44,22 @@ class GalleryService
             }, $imagesForAllGalleries);
             $imagesIds = array_values(array_unique($imagesIds));
             $this->imageGalleryRepository->InsertImagesForGallery($imagesIds, $createdGalleryId);
+        }
+    }
+
+    public function AddImagesToGallery($files, $fileQuality, $imagesDescription, $selectedTags, $galleryId)
+    {
+        if ($this->galleryUserValidatorService->canUserEditGallery($galleryId)) {
+            $this->imageValidationService->validateImages($imagesDescription, $files);
+            $savedImageNames = $this->imageUploadService->uploadImages($files, $fileQuality);
+
+            $user = $this->authorizationService->getLoggedInUser();
+            foreach ($savedImageNames as $savedImageName) {
+                $imageId = $this->imageRepository->Save($savedImageName, $imagesDescription, $user->id);
+                $this->imageGalleryRepository->Create($imageId, $galleryId);
+            }
+        } else {
+            throw new Exception("You cannot edit other peoples' galleries!", 400);
         }
     }
 }
