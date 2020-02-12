b8390439ca536e71d605ea2e4c3065a7eaa0e0bb
Zdravko Hvarlingov <hvurlingov@uni-sofia.bg>
2020-02-12 11:20:00 +0200

    Update compression for transparent png

--- Domain Layer/services/ImageUploadService.php
+++ Domain Layer/services/ImageUploadService.php
@@ -10,27 +10,30 @@ class ImageUploadService
         for ($i = 0; $i < count($fileNames); $i++) {
             $fileTmp = $files['tmp_name'][$i];
             $fileName = $files['name'][$i];
-
-            $fileInfo = getimagesize($fileTmp);
-            if($fileInfo['mime'] == 'image/jpeg') {
-                $image = imagecreatefromjpeg($fileTmp);
-            }
-            elseif ($fileInfo['mime'] == 'image/gif') {
-                $image = imagecreatefromgif($fileTmp);
-            }
-            elseif($fileInfo['mime'] == 'image/png') {
-                $image = imagecreatefrompng($fileTmp);
-            }
-
+            
             $fileExt = strtolower(explode(".", $fileName)[1]);
             $imageFullName = uniqid("", true) . "." . $fileExt;     // GUID
             $imageDestination = $this->imagesFolder . $imageFullName;
             if (!is_dir($this->imagesFolder)) {
                 mkdir($this->imagesFolder);
             }
-            
-            if($fileQuality == 100) move_uploaded_file($fileTmp, $imageDestination);    // upload the file
-            else imagejpeg($image, $imageDestination, $fileQuality);
+
+            $fileInfo = getimagesize($fileTmp);
+            if($fileInfo['mime'] == 'image/jpeg') {
+                $image = imagecreatefromjpeg($fileTmp);
+                $fileQuality = ($fileQuality / 100) * 90;
+
+                imagejpeg($image, $imageDestination, $fileQuality);
+            }
+            elseif($fileInfo['mime'] == 'image/png') {
+                $image = imagecreatefrompng($fileTmp);
+                imagealphablending($image, false);
+                imagesavealpha($image, true);
+                
+                $fileQuality = 9 - ($fileQuality / 100) * 8;
+                imagepng($image, $imageDestination, $fileQuality);
+            }
+
             array_push($imageNames, $imageFullName);
         }
 
